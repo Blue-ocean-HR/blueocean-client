bbc3a9d53b75506721385eb9f3c4b0366d3b6905
zrendy <zrendy@gmail.com>
2022-11-19 13:11:44 -0500

    Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import DummyPantry from './DummyPantry.jsx'
 import Nav from './Nav.jsx'
 import {Routes, Route, Link} from 'react-router-dom'
 import { useAuth0 } from "@auth0/auth0-react";
+import Recipes from './Recipes.jsx'
 const App = () => {
   const { isLoading } = useAuth0();
 
@@ -15,18 +16,15 @@ const App = () => {
   //   );
   // }
   return (
-    <div className="bg-slate-200 h-screen">
-    <div >
+    <div className="bg-orange-100 h-screen">
       <Nav />
       <Routes>
         <Route path="/" element={<DummyHome />} />
         <Route path="/pantry" element={<DummyPantry />} />
+        <Route path="/recipes" element={<Recipes/>}/>
         </Routes>
-
-
     </div>
-    </div>
-  )
-}
+  )}
+
 
 export default App;
