8fbc4afbbaf927a1c970f2fedbc69e68d79d7bec
zrendy <zrendy@gmail.com>
2022-11-19 14:01:10 -0500

    added choose ingredients

--- src/Recipes.jsx
+++ src/Recipes.jsx
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquarePlus, faThin} from '@fortawesome/free-solid-svg-icons'
 
 const Recipes = () => {
   const [query, setQuery] = useState('');
@@ -18,6 +20,20 @@ const Recipes = () => {
       <h1 className="text-primary text-4xl font-bold">RECIPES</h1>
       <input type="text" value={query} placeholder="Search.." onChange={handleInput}></input>
       <button type="submit" onSubmit={handleSearch}>Search</button>
+
+      {/* CHOOSE INGREDIENTS DROP DOWN */}
+      <div className="choose-ingredient">
+        <select name="ingredient" id="ingredient-dropdown">
+          {/* render pantry items accordingly */}
+          <option value="" disabled defaultValue>Choose Ingredients...</option>
+          <option value="pantry item 1">PANTRY 1</option>
+          <option value="pantry item 2">PANTRY 2</option>
+        </select>
+        <button><FontAwesomeIcon icon={faSquarePlus} /></button>
+      </div>
+
+
+
     </div>
   )
 }
