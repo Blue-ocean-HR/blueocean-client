989130fe7cde4d3be21b3e64c4acbeb7446d6b13
Jacky Thomas <jakejet64@gmail.com>
2022-11-22 17:33:14 -0500

    consolidated express server scripts to just npm start

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const path = require('path');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
